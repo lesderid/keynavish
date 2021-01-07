a72558e9291a54a47da5149e2f3f0c4251fa3ee7
Les De Ridder <les@lesderid.net>
2021-01-07 10:41:43 +0100

    Make grid colour slightly teal for improved visibility

--- src/keynavish/config.d
+++ src/keynavish/config.d
@@ -5,7 +5,7 @@ import core.sys.windows.windows;
 enum windowClassName = "keynavish-grid"w;
 enum windowColourKey = RGB(255, 0, 255);
 
-enum penColour = RGB(30, 30, 30);
+enum penColour = RGB(30, 64, 64);
 enum penWidth = 1;
 
 static assert(windowColourKey != penColour, "Colour key and pen colour can't be the same");
